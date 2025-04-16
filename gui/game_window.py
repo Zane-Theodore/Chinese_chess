e13acda778b486ce165e38daacc55de293ecfbdd
ThienPhuc <tthienphuc1612@gmail.com>
2025-04-16 15:29:15 +0700

    Fix the typos

--- gui/game_window.py
+++ gui/game_window.py
@@ -1,8 +1,8 @@
 import pygame
-from utils.config import HEIHGT, WIDTH, tittle
+from utils.config import HEIGHT, WIDTH, tittle
 
-def create_windown(width = WIDTH, height = HEIHGT, caption = tittle):
+def create_windown(width = WIDTH, height = HEIGHT, caption = tittle):
     pygame.init()
-    screen = pygame.display.set_mode((width, height))
+    screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
     pygame.display.set_caption(caption)
     return screen
